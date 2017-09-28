6ca8230d8b8699329db1c8bbef1a6404a01c59c7
LibertT <LibertT@LMC-4-15.s2.rpn.ch>
2017-09-28 15:27:49 +0200

    full responsive mofo

--- index.php
+++ index.php
@@ -27,19 +27,22 @@
     <link rel="stylesheet" href="styles/style.css"/>
 </head>
 <body>
-<div id="background-opacity"></div>
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-xs-11 col-sm-10 col-md-8 col-lg-8">
-            <h1>Yoda Speak</h1>
-            <div class="row justify-content-center">
-            <form class="col-xs-10 col-sm-8 col-md-8 col-lg-8" action="index.php" method="post">
+    <div id="background-opacity"></div>
+    <div class="container">
+        <!-- Header -->
+        <header class="row justify-content-center">
+            <h1 class="col-12">Yoda Speak</h1>
+        </header>
+        <!-- Form row -->
+        <section class="row"> 
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
                 <p>
-                    <label for="idTextToConvert">Vous dites: (<?php
-                        echo MAX_LENGTH;?> caractères maximum)
-                    </label>
+                    Tempore quo primis auspiciis in mundanum fulgorem surgeret victura dum erunt homines Roma, ut augeretur sublimibus incrementis, foedere pacis aeternae Virtus convenit atque Fortuna plerumque dissidentes, quarum si altera defuisset, ad perfectam non venerat summitatem.
                 </p>
-                <p>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                <form action="index.php" method="post">
+                    <label>Votre texte (maximum 140 caractères)</label>
                     <textarea id="idTextToConvert" name="v_TextToConvert"
                              rows="4"
                              maxlength="<?php echo MAX_LENGTH;?>"
@@ -48,43 +51,55 @@
                         if (isset($textToConvert)){
                             echo $textToConvert;
                         }
-                    ?></textarea></p>
-                <button type="submit" name="v_Convert">Convertir</button>
-            </form>
+                    ?></textarea>
+                    <button type="submit" name="v_Convert">Convertir</button>
+                </form>
             </div>
-           
-            <?php       
-            if (!empty($textToConvert)){
-                // retrieves json from corenlp server
-                $json = postRequest($textToConvert);
+        </section>        
+        <?php       
+        if (!empty($textToConvert)){
+            // retrieves json from corenlp server
+            $json = postRequest($textToConvert);
 
-                // convert it to an array of elements with pos
-                $elements = jsonToElements($json);
+            // convert it to an array of elements with pos
+            $elements = jsonToElements($json);
 
-                if (DEBUG){
-                    // print json
-                    echo '<h2>JSON:</h2><p>' . $json . '</p>';
-                    // print annotated sentence
-                    echo '<h2>Phrase annotée:</h2><p>';
-                    foreach ($elements as $element){
-                        echo $element->getWord() . '('. $element->getPOS(). ') ';
-                    }
-                    echo '</p>';
-                }       
-                echo
-                    '<div id="bubble">
-                    <span id="text">' . convert($elements) . '</span>
-                    <span id="arrow_border"></span>
-                    <span id="arrow_inner"></span>
-                    </div>';
+            if (DEBUG){
+                echo '<div class="row"><div class="col-12">';
+                // print json
+                echo '<h2>JSON:</h2><p>' . $json . '</p>';
+                // print annotated sentence
+                echo '<h2>Phrase annotée:</h2><p>';
+                foreach ($elements as $element){
+                    echo $element->getWord() . '('. $element->getPOS(). ') ';
+                }
+                echo '</p>';
+                echo '</div></div>';
             }
-            echo '<img class="col-5" id="yoda" src="images/dank-yoda.png"/>';
-            ?>
-        </div>
+            
+            // random number to display a random img of yoda
+            $numImg = rand(1, 4);          
+            echo
+            '<div class="row justify-content-center">
+                <div class="col-xs-12 col-sm-8 col-md-6 col-lg-4" id="bubble">
+                <span id="text">' . convert($elements) . '</span>
+                <span id="arrow_border"></span>
+                <span id="arrow_inner"></span>
+            </div>                 
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-xs-8 col-sm-8 col-md-6 col-lg-4">
+                    <img id="yoda" src="images/yoda-0'. $numImg . '.png"/>
+                </div>
+            </div>';           
+        }   
+        ?>               
+        <!-- Footer -->
+        <footer class="row">
+            <div class="col-12">
+                Copyright © <?php echo date("Y") ?> | Thibault Libert
+            </div>    
+        </footer>
     </div>
-    <footer>
-        <p>Copyright - <?php echo date("Y") ?></p>
-    </footer>
-</div>
 </body>
 </html>
